ae9f83a5b1a0f51d1f8f71e9271247ab2eec31a1
jordenhl <jordenhl@gmail.com>
2017-01-11 20:33:25 +0200

    add change '/hello' to '/'

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/hello', function (req, res) {
+app.get('/', function (req, res) {
    let name = req.query.name || "<unknown>";
    res.send(`Hello there ${name}`);
 });
