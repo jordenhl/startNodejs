9076c969b2ea167ebbab1539fe19f886734ddf7f
jordenhl <jordenhl@gmail.com>
2017-01-11 18:59:37 +0200

    start project2

--- test.js
+++ test.js
@@ -1,9 +1,14 @@
 var express = require('express');
- var app = express();
- app.get('/hello', function (req, res) {
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/hello', function (req, res) {
    let name = req.query.name || "<unknown>";
    res.send(`Hello there ${name}`);
 });
- app.listen(3000, function () {
+
+
+app.listen(app.get('port'), function () {
    console.log('Listening on port 3000!');
 });
